5493e26c8024ac20339775c17d18280effaa0c9b
Izak Van Heerden <izakvanheerden1@gmail.com>
2023-03-13 14:27:22 +0200

    redit the testing backend

--- AnimuSharp.Tests/ClientTests.cs
+++ AnimuSharp.Tests/ClientTests.cs
@@ -1,38 +1,52 @@
-﻿using animuSharp.Client;
+﻿using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using animuSharp.Client;
 using animuSharp.Client.Internals.Enums;
+using NSubstitute.Core;
+using static AnimuSharp.Tests.ClientTests;
 
 namespace AnimuSharp.Tests
 {
     public class ClientTests
     {
-        public const string apikey = "";
+        public ClientTests()
+        {
+        }
+
+        private const string apikey = "76a65670f12f6f6546b24c90335b88456ee5e864fce3";
+
+        private Client CreateClient()
+        {
+            return new Client(apikey);
+        }
 
         [Fact]
         public async Task GetURl_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
-            var client = new Client(apikey);
+            var client = this.CreateClient();
+            ContentType content = ContentType.angry;
 
             // Act
-            var result = await client.GetURl(ContentType.angry);
-            var url = result.Url;
+            var result = await client.GetURl(content);
 
             // Assert
-            Assert.NotNull(url);
+            Assert.NotNull(result);
         }
 
         [Fact]
         public async Task GetwaifuURl_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
-            var client = new Client(apikey);
+            var client = this.CreateClient();
 
             // Act
             var result = await client.GetwaifuURl();
-            var wurl = result.Names.En;
 
             // Assert
-            Assert.NotNull(wurl);
+            Assert.NotNull(result);
         }
     }
 }
