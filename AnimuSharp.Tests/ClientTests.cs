962be69e5e5e19ab688a75636be11e29ca975ab3
Izak Van Heerden <izakvanheerden1@gmail.com>
2023-07-25 20:34:58 +0200

    redid the tersting backend

--- AnimuSharp.Tests/ClientTests.cs
+++ AnimuSharp.Tests/ClientTests.cs
@@ -1,29 +1,63 @@
-﻿using animuSharp.Client;
-using animuSharp.Client.Internals.DataTypes;
-using animuSharp.Client.Internals.Enums;
+﻿using System;
+using System.Threading.Tasks;
 using Xunit;
+using animuSharp.ClientClass;
+using animuSharp.ClientClass.Internals.Enums;
+using animuSharp.ClientClass.Internals.DataTypes;
+using System.Diagnostics;
 
-namespace AnimuSharp.Tests
+namespace animuSharp.Tests
 {
     public class ClientTests
     {
-        public ClientTests()
-        {
-        }
-
-        private const string apikey = "";
+        private const string apikey = "ee576a68fd737d63ca49ccdd9a6ed165c0de2146fa39";
 
         [Fact]
-        public async Task GetURl_StateUnderTest_ExpectedBehavior()
+        public async Task GetGenericURl_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
             Client client = new Client(apikey);
 
             // Act
-            var result = await client.GetURl(ContentType.quote);
+            var result = await client.GetGenericURl(ImageContentType.angry);
+
+            var item = result.Url;
 
             // Assert
-            Assert.NotNull(result);
+            Debug.WriteLine(item);
+            Assert.NotNull(item);
+        }
+
+        [Fact]
+        public async Task GetTextURl_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            Client client = new Client(apikey);
+
+            // Act
+            var result = await client.GetTextURl(Textypes.owoify, "Hello world");
+
+            var item = result.TText;
+
+            // Assert
+            Debug.WriteLine(item);
+            Assert.NotNull(item);
+        }
+
+        [Fact]
+        public async Task GetMiscURl_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            Client client = new Client(apikey);
+
+            // Act
+            var result = await client.GetMiscURl<Data.Waifu>(Misc.Waifu);
+
+            var item = result.Names;
+
+            // Assert
+            Debug.WriteLine(item);
+            Assert.NotNull(item);
         }
     }
 }
